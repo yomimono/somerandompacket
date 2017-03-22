95841621290a4a27a737be54fbb3378285ce31a7
Mindy Preston <mindy.preston@docker.com>
2017-03-22 13:54:41 -0500

    don't be quite so silly about getting input

--- src/fuzz.ml
+++ src/fuzz.ml
@@ -1,8 +1,19 @@
-let main ~f ~pp () =
+let get_input () =
   set_binary_mode_in stdin true;
+  let b = Buffer.create 1000 in
+  let buf = Bytes.create 1000 in
   try
-    match f @@ Cstruct.of_string @@ read_line () with
-    | Ok (h, _p) -> Format.printf "%a" pp h
-    | Error e -> Format.printf "%s" e
-  with
-  | End_of_file -> Printf.eprintf "No more input available, giving up"; exit 1
+    while true do
+      let got = input stdin buf 0 (Bytes.length buf) in
+      if got = 0 then raise End_of_file;
+      assert (got > 0);
+      Buffer.add_subbytes b buf 0 got
+    done;
+    assert false
+  with End_of_file ->
+    Buffer.contents b
+
+let main ~f ~pp () =
+   match f @@ Cstruct.of_string @@ get_input () with
+   | Ok (h, _p) -> Format.printf "%a" pp h
+   | Error e -> Format.printf "%s" e
